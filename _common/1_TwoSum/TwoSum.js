fd63b3247cffd9caedaa42b5c799c13e0dbcda22
Vladimir Gordeev <wsg434@gmail.com>
2022-10-31 09:21:28 +0300

    Task #1 TwoSum; JS

---
+++ _common/1_TwoSum/TwoSum.js
@@ -0,0 +1,35 @@
+
+//---------------------------Task condition
+//Task #1 Two Sum
+//https://leetcode.com/problems/two-sum/
+
+
+//---------------------------Test cases
+// nums = [2,7,11,15];
+// nums = [3,2,4];
+nums = [3, 3];
+nums;
+// target  = 9;
+// target  = 6;
+target = 6;
+target;
+currentSum = 0;
+result = [0, 0];
+
+
+//---------------------------Solution
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        let result = [i, j];
+        return result;
+      }
+    }
+  }
+  return false;
+}
+
+//---------------------------Check result
+k = twoSum(nums, target);
+k;
